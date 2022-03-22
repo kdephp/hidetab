74fd26865424d3c244a0ed7e00b7ab3c9a7e0eb8
ELESSARX <kdephp@gmail.com>
2022-03-22 13:00:45 +0200

    Adding comments

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+// Arrays for example
+
 $infoHeaderTab = array('Treatment', 'Rest', 'Nature', 'Yoga');
 
 $infoTab = array(
@@ -31,38 +33,56 @@ $infoTab = array(
 <!DOCTYPE html>
 <html lang="ru">
 <head>
+    <!-- Required meta tags -->
 	<meta charset="UTF-8">
 	<title>Hide tab</title>
-
+    <!-- Google Fonts -->
 	<link href="https://fonts.googleapis.com/css?family=Roboto+Slab:300,400,700&amp;subset=cyrillic-ext" rel="stylesheet">
+    <!-- Bootstrap min CSS -->
     <link rel="stylesheet" href="css/bootstrap-grid.min.css">
+    <!-- Style CSS -->
 	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+    <!-- Start content -->
 	<div class="content">
+        <!-- Start container -->
 		<div class="container" id="about">
+            <!-- Start info -->
 			<div class="info" >
+                <!-- Start info header -->
 				<div class="info-header">
+                    <!-- Start foreach header tab-->
                     <?php foreach ($infoHeaderTab as $tab) { ?>
                         <div class="info-header-tab"><?php echo $tab ?></div>
+                    <!-- End foreach header tab-->
                     <?php } ?>
+                <!-- End info header -->
 				</div>
+                    <!-- Start foreach tabcontent-->
                     <?php foreach ($infoTab as $tab => $key) { ?>
 				<div class="info-tabcontent fade">
 					<div class="description">
+                        <!-- Echo description title -->
 						<div class="description-title"><?php echo $tab ?></div>
+                        <!-- Echo description text -->
                         <div class="description-text"><?php echo $key ?></div>
 						<div class="description-btn">Learn more</div>
 					</div>
 					<div class="photo">
+                        <!-- Image on the right -->
 						<img src="img/<?php echo str_replace(' ', '_', $tab); ?>.jpg" alt="Massage">
 					</div>
 				</div>
+                    <!-- End foreach tabcontent-->
                     <?php } ?>
-
+                <!-- End info -->
 				</div>
+            <!-- End container -->
 			</div>
+    <!-- End content -->
     </div>
+    <!-- script js -->
 	<script src="js/script.js"></script>
 </body>
 </html>
--- js/script.js
+++ js/script.js
@@ -1,19 +1,23 @@
+/*HTML has been fully loaded*/
 window.addEventListener('DOMContentLoaded', function (){
 
     'use strict';
+
     let tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
 
+    // Hide tab
     function hideTabContent(a) {
         for (let i = a; i <tabContent.length; i++){
             tabContent[i].classList.remove('show');
             tabContent[i].classList.add('hide');
         }
     }
-
+    // Call hide tab function
     hideTabContent(1);
 
+    // Show tab
     function showTabContent(b){
         if (tabContent[b].classList.contains('hide')){
             tabContent[b].classList.remove('hide');
@@ -21,10 +25,14 @@ window.addEventListener('DOMContentLoaded', function (){
         }
     }
 
+    // Menu click tracking
     info.addEventListener('click', function (event){
+        // Click recording
         let target = event.target;
         if (target && target.classList.contains('info-header-tab')) {
+            // Listing all tabs
             for(let i = 0; i < tab.length; i++){
+                // Checking for matches with a click
                 if (target == tab[i]){
                     hideTabContent(0);
                     showTabContent(i);
